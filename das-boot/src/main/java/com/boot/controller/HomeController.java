bc463bd7fcc3e2efe9ef32d9fcfd8187dd0ee12e
Kristijan <ilievk@vmware.com>
2017-06-18 13:44:31 +0300

    initial setup for Shipwreck app tutorial

---
+++ das-boot/src/main/java/com/boot/controller/HomeController.java
@@ -0,0 +1,17 @@
+package com.boot.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Hello world!
+ *
+ */
+@RestController
+public class HomeController {
+
+	@RequestMapping("/")
+	public String home() {
+		return "Das boot!";
+	}
+}
